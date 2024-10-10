743c01a614fd1691b1e96ea043a9aa62e2b0d354
Jareczeq <contact@jareczeq.pl>
2024-10-10 19:02:52 +0200

    Usunięto odstępy w kodzie

--- kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -15,7 +15,6 @@ public class WeatherForecast {
 
     public Map<String, Double> calculateForecast() {
         Map<String, Double> resultMap = new HashMap<>();
-
         for (Map.Entry<String, Double> temperature : temperatures.getTemperatures().entrySet()) {
             resultMap.put(temperature.getKey(), temperature.getValue() + 1.0);
         }
@@ -24,9 +23,7 @@ public class WeatherForecast {
     }
 
     public double calculateAverageTemperature() {
-
         double avg = 0;
-
         for (double temperature : temperatures.getTemperatures().values()) {
             avg += temperature;
         }
