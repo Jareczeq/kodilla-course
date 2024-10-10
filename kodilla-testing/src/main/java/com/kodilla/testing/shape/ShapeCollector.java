6a057282505d3418e16470088b342048a9e2cedf
Jareczeq <contact@jareczeq.pl>
2024-10-10 17:03:44 +0200

    Dodano figury geometryczne oraz klasę, która będzie je przechowywać

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,41 @@
+package com.kodilla.testing.shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShapeCollector {
+
+    private List<Shape> shapes = new ArrayList<>();
+
+    public void addShape(Shape shape) {
+        shapes.add(shape);
+    }
+
+    public void removeShape(Shape shape) {
+        shapes.remove(shape);
+    }
+
+    public Shape getFigure(int n) {
+        return shapes.get(n);
+    }
+
+    public String showFigures() {
+
+        if (shapes.isEmpty()) {
+            return "There are no shapes!";
+        }
+
+        Shape firstShape = shapes.get(0);
+        StringBuilder sb = new StringBuilder("Shapes: " + firstShape.getShapeName());
+
+        for (int i = 1; i < shapes.size(); i++) {
+            Shape shape = shapes.get(i);
+            sb.append(", " + shape.getShapeName());
+        }
+
+
+        return sb.toString();
+
+    }
+
+}
