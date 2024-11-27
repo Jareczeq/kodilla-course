ebff7a4c3070bd62af34a6e324b12bebd8cd1689
Jareczeq <contact@jareczeq.pl>
2024-11-27 07:18:26 +0100

    Zrobiono zadanie 14.2

---
+++ kodilla-patterns/src/main/java/pl/jareczeq/patterns/prototype/library/Library.java
@@ -0,0 +1,63 @@
+package pl.jareczeq.patterns.prototype.library;
+
+import pl.jareczeq.patterns.prototype.Prototype;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public final class Library extends Prototype<Library> {
+
+   private String name;
+   private Set<Book> books = new HashSet<>();
+
+    public Library(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = super.clone();
+        clonedLibrary.books = new HashSet<>();
+
+        for (Book book : books) {
+            clonedLibrary.books.add(book);
+        }
+
+        return clonedLibrary;
+    }
+
+    @Override
+    public String toString() {
+        return "Library { " + name + " }";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return Objects.equals(name, library.name) &&
+                Objects.equals(books, library.books);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, books);
+    }
+}
